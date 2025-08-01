66445d3b21e67a15e156c1ca71f865d965f811a9
Abry <abrywandarful@gmail.com>
2025-08-01 14:13:36 +0700

    Add files via upload

---
+++ KelasXR5.java
@@ -0,0 +1,24 @@
+package com.codewithatha;
+
+
+public class KelasXR5 {
+    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("where do u live?");
+//        String name = scanner.nextLine();
+//
+//        if (name == "Jakarta"){
+//            System.out.println("You are underaged");
+//        }else
+//            System.out.println("You are not underaged");
+
+        System.out.println("R Muhammad Aubrey Athalah Widjaya");
+        System.out.println("SMP Muhammadiyah 9 Jakarta");
+        System.out.println("Cilandak, jalan Madrasah, Jakarta Selatan");
+        System.out.println("Coding, Volley");
+        System.out.println("Programmer handal");
+        System.out.println("15 Tahun");
+
+    }
+}
